1e02aba4c1f096f67d831b9b83c558ac738fd545
RM <devRMoreira@gmail.com>
2024-04-12 18:03:05 +0100

    #

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,20 +16,23 @@ export default function Home() {
 
 
     </div>
+
     {state === "" ? <div className="flex flex-col h-svh w-1/2 justify-center items-center ">
-      
+
       <h1>Join what's going on in the world today.</h1>
 
       <button className="text-center border mb-10 w-24 rounded-lg bg-violet-400 border-violet-900 text-white"
         onClick={() => handleClick("reg")}> Register </button>
 
+
       <h2>Already with us?</h2>
+
       <button className="text-center border w-24 rounded-lg border-violet-400"
         onClick={() => handleClick("log")}> Login </button>
 
     </div> : state === "log" ? <Login /> : state === "reg" ? <Register /> : undefined}
 
-  </div> 
+  </div>
 
 
 }
