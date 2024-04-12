2630383a6b51dcdd2acfdae900420d66fe2eddac
RM <devRMoreira@gmail.com>
2024-04-12 17:44:40 +0100

    endpoints

--- src/pages/api/auth/login.js
+++ src/pages/api/auth/login.js
@@ -1,10 +1,7 @@
-import { connectToMongo, getMongoCollection } from "@/backend/data/mongo";
-
 export default function handler(req, res) {
 
     if(req.method === "POST"){
 
-        connectToMongo()
 
         res.end()
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,34 @@
+import { useState } from "react"
+
 export default function Home() {
 
-  return <div>
-      <button> Login </button>
-      
+  const [state, setState] = useState("")
+
+  function handleClick(display) {
+    setState(display)
+  }
+
+  return <div className="flex">
+
+    <div className="w-1/2">
+
+
+    </div>
+
+    <div className="flex flex-col h-svh w-1/2 justify-center items-center ">
+
+      <h1>Join what's going on in the world today.</h1>
+
+      <button className="text-center border mb-10 w-24 rounded-lg bg-violet-400 border-violet-900 text-white"
+        onClick={() => handleClick("reg")}> Register </button>
+
+      <h2>Already with us?</h2>
+      <button className="text-center border w-24 rounded-lg border-violet-400"
+        onClick={() => handleClick("log")}> Login </button>
+
+    </div>
+
   </div>
+
+
 }
