38f72c7142eca395f2225b287d4dd20c9afa36b9
RM <devRMoreira@gmail.com>
2024-04-15 15:36:07 +0100

    #

--- src/frontend/components/buttons/MainButton.jsx
+++ src/frontend/components/buttons/MainButton.jsx
@@ -1,5 +1,5 @@
 
-export default function MainButton({ text, onClick, color }) {
+export default function MainButton({ text, color, onClick }) {
 
     if (color === "dark") {
         return <button onClick={onClick} className="text-center border w-24 rounded-lg bg-violet-400 border-violet-900 text-white"> {text} </button>
