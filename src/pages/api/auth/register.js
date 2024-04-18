15a6acb472721caacd52d0eda39cb1785ee24809
RM <devRMoreira@gmail.com>
2024-04-18 21:51:36 +0100

    backend route logic for a new user partially implemented

--- src/pages/api/auth/register.js
+++ src/pages/api/auth/register.js
@@ -1,12 +1,32 @@
-export default function handler(req, res) {
+import { registerNewUser } from "@/backend/data/user/registration"
+import { birthDateIsValid, hashPassword } from "@/backend/services/user/utils"
 
-  if (req.method === "POST") {
+export default async function handler(req, res) {
 
-    const userRegister = req.body
-    console.log(userRegister)
-    res.json(userRegister)
+	if (req.method === "POST") {
 
-  }
+		if (req.body.password !== req.body.passwordConfirmation) {
+			res.send(400).end()
+		}
 
+		const newUser = {
+			name: req.body.name,
+			email: req.body.email,
+			password: await hashPassword(req.body.password),
+			birthDate: req.body.birthDate
+		}
+
+		//TODO Add newUser.name validation (unique or not)
+
+
+		if (!birthDateIsValid(newUser.birthDate)) {
+			res.send(400).end()
+		}
+
+		await registerNewUser(newUser)
+
+		res.json(newUser)
+
+	}
 
 }
